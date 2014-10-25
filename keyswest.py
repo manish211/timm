b01fa139ecdf1f5788fccff710c7d619bee79664
timm <tim@menzies.us>
2014-10-25 00:20:37 -0400

    working on keyswest

--- keyswest.py
+++ keyswest.py
@@ -1,68 +1,52 @@
 from __future__ import division,print_function
-import sys,random
+import sys,random,re
 sys.dont_write_bytecode =True
 
 def settings(**d): return o(
-  name="DEADANT v0.1",
-  what="A stochastic multi-objective tabu active learner",
+  name="KEYSWEST v0.1",
+  what="A linear-time MOEA/D variant",
   synopsis="""
-    DeadAnt's tabu memory is a trail of 'dead ants' in
-    regions known to be sub-optimal. Landing too close
-    to a dead ant will 'kill' any new candidate.  To
-    reduce the overhead of searching through the ants,
-    pairs of close dead ants are incrementally fused
-    together.  To better explore good solutions, if a
-    candidate is dominated by a live ant, then the
-    candidate is nudged towards the better ant. This is
-    an active learner since new solutions are only
-    evaluated if are not 'close' to a dead ant (where
-    'close' is learned dynamically).""",
+  Read tabular data from disk.  Find 'islands'-
+  leaves of a dendogram found via recursive PCA-like
+  clustering. Then discretize the numerics via
+  minimizing the entropy of the island ids
+  associated with the numeric values.  Apply 'envy'
+  to find islands 'better' than some 'current'
+  island.  Sort ranges via their frequency in better
+  and current.  Greedy search the sort to find the
+  best fewest ranges.
+  """,
   _logo="""
-             "=.
-             "=. \ 
-                \ \ 
-             _,-=\/=._        _.-,_
-            /         \      /=-._ "-.
-           |=-./~\___/~\    / How `-._\ 
-           |   \o/   \o/   /  to      /
-            \_   `~~~;/    |  Dodge   |
-              `~,._,-'    /   Dead   /
-                 | |      =-._Things/
-             _,-=/ \=-._     /|`-._/
-           //           \\   )\ 
-          /|    I See   |)_.'/
-         //|    Dead    |\_."   _.-\ 
-        (|  \   People /    _.`=    \ 
-        ||   ":_    _.;"_.-;"   _.-=.:
-     _-."/    / `-."\_."        =-_.;\ 
-    `-_./   /             _.-=.    / \\ 
-           |              =-_.;\ ."   \\ 
-           \                   \\/     \\ 
-           /\_                .'\\      \\ 
-          //  `=_         _.-"   \\      \\ 
-         //      `~-.=`"`'       ||      ||
-   LGB   ||    _.-_/|            ||      |\_.-_
-     _.-_/|   /_.-._/            |\_.-_  \_.-._\ 
-    /_.-._/                      \_.-._\ 
+            .-""-.
+           / .--. \ 
+          / /    \ \ 
+          | |    | |
+          | |.-""-.|
+         ///`.::::.`\ 
+        ||| ::/  \:: ;
+        ||; ::\__/:: ;
+         \\\ '::::' /
+     jgs  `=':-..-'`
     """,
-  author="Tim Menzies, Rahul Krishna",
+  author="Tim Menzies",
   copyleft="(c) 2014, MIT license, http://goo.gl/3UYBp",
   seed=1,
-  np=10,
-  k=100,
-  kMore=1.1,
-  tiny=0.05,
+  tiny=0.5,
   start='print(The._logo)',
-  closeEnough=2,
-  de=o(np=10,
-       f=0.3,
-       cf=0.4,
-       lives=9)
+  cache = o(size=256),
+  reader= o(sep      = ",",
+            bad      = r'(["\' \t\r\n]|#.*)',
+            skip     ='?',
+            numc     ='$',
+            missing  = '?',
+            )
   ).update(**d)
 
 class o:
   def __init__(i,**d): i.update(**d)
   def update(i,**d): i.__dict__.update(**d); return i
+
+The= settings()
 
 rand= random.random
 seed= random.seed
@@ -76,8 +60,6 @@ def sayln(*lst):
 
 def _say(): sayln(1,2,3,4)
 
-The= settings()
-
 def cmd(com=The.start):
   if globals()["__name__"] == "__main__":
     if len(sys.argv) == 3:
@@ -87,41 +69,97 @@ def cmd(com=The.start):
         com = sys.argv[2] + '(' + sys.argv[3] + ')'
     eval(com)
 
-class Close():
-  def __init__(i):
-    i.sum, i.n = [0]*32, [0]*32
-  def p(i,x):
-    for j in xrange(len(i.sum)):
-      mu = i.sum[j] / i.n[j]
-      if x > mu:
-        return i.n[j]/i.n[0]
-    return i.n[-1]/i.n[0]
-  def __iadd__(i,x):
-    for j in xrange(len(i.sum)):
-      i.sum[j] += x
-      i.n[j]   += 1
-      mu        = i.sum[j] / i.n[j]
-      if x >= mu: return i
-      if i.sum[j] < The.closeEnough: return i
-    return i
-  def close(i,x):
-    return i.p(x) < The.tiny
+def medianIQR(lst, ordered=False):
+  lst = lst if ordered else sorted(lst)
+  n   = len(lst)
+  q   = n//4
+  iqr = lst[q*3] - lst[q]
+  if n % 2:  return lst[q*2],iqr
+  p = max(0,q-1)
+  return (lst[p] + lst[q]) * 0.5,iqr
 
-def _close(n=10000,p=2,rseed=None):
-  seed(rseed or The.seed)
-  cl=Close()
-  for _ in xrange(n):
-    cl += rand()*100
-  print(':p',cl.p(p),':close',cl.close(p))
-  print(map(lambda x: int(x[0]/x[1]) if x[1] else 0,zip(cl.sum,cl.n)))
+def atom(x):
+  try : return int(x)
+  except ValueError:
+    try : return float(x)
+    except ValueError : return x
 
+#########################################################
+
+def table(source):
+  tbl = o(rows=[],cols=None)
+  for n,cells in row(source): 
+    if n == 0 : 
+      f = factory(cells)
+      tbl.cols = f(); exit()
+    else : 
+      tbl.cols += cells
+  return tbl
+
+def row(file):
+  """Leaps over any columns marked 'skip'.
+  Turn strings to numbers or strings. 
+  Kill comments. Join lines that end in 'sep'."""
+  skip = The.reader.skip
+  sep  = The.reader.sep
+  bad  = The.reader.bad
+  def rows(): 
+    n,kept = 0,""
+    for line in open(file):
+      now   = re.sub(bad,"",line)
+      kept += now
+      if kept:
+        if not now[-1] == sep:
+          yield n, map(atom, kept.split(sep))
+          n += 1
+          kept = "" 
+  todo = None
+  for n,line in rows():
+    todo = todo or [col for col,name in enumerate(line) 
+                    if not skip in name]
+    yield n, [ line[col] for col in todo ]
+
+def _rows(f='data/nasa93.csv'):
+   for n,cells in row(f): 
+     print(n,cells)
+
+
+
+#########################################################
 class Col:
   def any(i): return None
-  def fuse(i,x,w1,y,w2): return None
-  def nudge(i,x,y): return None
   def dist(i,x,y): return 0
   def norm(i,x) : return x
-  def extrapolate(i,x,y,z): return None
+
+class Cache:
+  "Keep a random sample of stuff seen so far."
+  def any(i): return any(i.all)
+  def dist(i,x,y): 
+    return i.norm(x) - i.norm(y)
+  def norm(i,x):
+    has = i.has()
+    return (x- has().lo) / (has.hi- has.lo + 0.00001)
+  def __init__(i,inits=[]):
+    i.all,i.n,i._has = [],0,None
+    map(i.__iadd__,inits)
+  def __iadd__(i,x):
+    i.n += 1
+    if len(i.all) < The.cache.size: # if not full
+      i._has = None
+      i.all += [x]               # then add
+    else: # otherwise, maybe replace an old item
+      if rand() <= The.cache.size/i.n:
+        i._has=None
+        i.all[int(rand()*The.cache.size)] = x
+    return i
+  def has(i):
+    if i._has == None:
+      lst  = sorted(i.all)
+      med,iqr = medianIQR(i.all,ordered=True)
+      i._has = o(
+        median = med,      iqr = iqr,
+        lo     = i.all[0], hi  = i.all[-1])
+    return i._has
 
 class N(Col):
   "For nums"
@@ -130,11 +168,6 @@ class N(Col):
     i.name=None
     i.least, i.most=least,most  
     i.lo,i.hi = 10**32, -1*10**32
-  def extrapolate(i,x,y,z):
-    f=The.de.f
-    return x + f*(y - z) if rand() < The.de.cf else x    
-  def any(i):
-    return i.least + rand()*(i.most - i.least)
   def __iadd__(i,x):
     assert x >= i.least and x <= i.most
     i.lo = min(i.lo,x)
@@ -145,13 +178,6 @@ class N(Col):
     return max(0,min(1,tmp))
   def dist(i,x,y):
     return i.norm(x) - i.norm(y)
-  def fuse(i,x,w1,y,w2):
-    return (x*w1 + y*w2)/(w1+w2)
-  def nudge(i,x,y):
-    tmp = x + rand()*1.5*(y-x)
-    if tmp > i.most : tmp = i.least
-    if tmp < i.least: tmp = i.most
-    return tmp
     
 class S(Col):
   "For syms"
@@ -165,16 +191,6 @@ class S(Col):
   def __iadd__(i,x): 
     assert x in i.index
   def dist(i,x,y): return 0 if x == y else 0
-  def fuse(i,x,w1,y,w2):
-    return x if rand() <= w1/(w1+w2) else y
-  def nudge(i,x,y):
-    return x if rand() < 0.33 else y
-  def extrapolate(i,x,y,z):
-    if rand() >= The.de.cf: 
-      return x
-    else:
-      w = y if rand() <= f else z 
-      return x if rand() <= 0.5 else w
 
 class O(Col):
   "for objectives"
@@ -201,37 +217,24 @@ class O(Col):
   
 class Meta(Col):
   id=0
-  def __init__(i,of,weight=1,dead=True):
-    i.weight, i.dead,i.of = weight,dead,of
+  def __init__(i,of,weight=1):
+    i.weight, i.of, i.klass = weight, of, None
     i.id = Meta.id = Meta.id + 1
   def any(i):
     return Meta(i.of)
-  def fuse(i,x,w1,y,w2): 
-    tmp = i.any()
-    tmp.weight = w1+w2
-    return tmp
-  def nudge(i): return i.any()
   def __repr__(i):
     return i.of.name + ':' \
            + ('DEAD' if i.dead else 'ALIVE') \
            + '*' + str(i.weight)
 
-def Schaffer():
-  def f1(row): return row[1]**2
-  def f2(row): return (row[1]-2)**2
-  return Cols(Schaffer,
-                 [N(least=-4, most=4)
-                 ,O(f=f1)
-                 ,O(f=f2)
-                 ])
+def head(lst):
+  def w(n,x):
+    y = N if The.reader.numc in x else S
+    y = y()
+    y.name, y.col = n, x
+  return [w(n,x) for n,x in enumerate(lst)]
 
-def _schaffer():
-  m=Schaffer()
-  for _ in range(10):
-    one = m.any()
-    m.score(one)
-    print(one)
-  
+
 class Cols:
   def __init__(i,factory,cols=[]):
     i.cols = [Meta(i)] + cols
@@ -248,20 +251,6 @@ class Cols:
   def tell(i,lst): 
     for z in i.indep: z += lst[z.col]
   def score(i,l): return [z.score(l) for z in i.objs]
-  def nudge(i,lst1,lst2):
-    return [one.nudge(x,w1,y,w2) 
-            for x,y,one in vals(lst1,lst2,i.cols)]
-  def extrapolate(i,lst1,lst2,lst3):
-    tmp= [one.extrapolate(x,y,z) for x,y,z,one in 
-            vals3(lst1,lst2,lst3,i.cols)]
-    one = any(i.objs)
-    tmp[one.col] = lst1[one.col]
-    return tmp
-  def fuse(i,lst1,lst2):
-    w1= lst1[0].weight
-    w2= lst2[0].weight
-    return [one.fuse(x,w1,y,w2) 
-            for x,y,one in vals(lst1,lst2,i.cols)]
   def fromHell(i):
     x,c = 0, len(i.objs)
     for col in i.objs:
@@ -285,6 +274,11 @@ class Cols:
     if not peeking: cl += d          
     return d
 
+def factory(lst):
+  def f():
+    return Cols(f,cols=head(lst))
+  return f
+
 def vals(lst1,lst2,cols):
   for c in cols:
     yield lst1[c.col],lst2[c.col],c
@@ -297,95 +291,10 @@ def fromLine(a,b,c):
     x = (a**2 + c**2 - b**2)/ (2*c)
     return max(0,(a**2 - x**2))**0.5
 
-def neighbors(m,lst1,pop):
-  return sorted([(m.dist(lst1,lst2),lst2) 
-                 for lst2 in pop 
-                 if not lst1[0].id == lst2[0].id])
+tbl = table('data/nasa93.csv')
 
-def deadAnt(model):
-  m   = model()
-  pop = {}
-  def remember(new): m += new; pop[ new[0].id ]= new
-  def itsAlive(lst): lst[0].dead = False; return lst
-  def itsDead(lst) : lst[0].dead = True;  return lst
-  def itsGone(lst) : del pop[lst[0].id]
-  def makeSomeAnts(n) :
-    for _ in range(n):
-      remember( itsAlive( m.any() ))
-  makeSomeAnts( The.np*len(m.indep) )
-  k   = The.k
-  new = m.any()
-  while k > 0:
-    k -= 1
-    (a,old),(b,other) = neighbors(m,new,
-                                  pop.values())[:1]
-    if not m.cl.close(a):
-      c = m.dist(old,other,peeking=True)
-      y = fromLine(a,b,c)
-      if not m.cl.close(y):
-        remember( itsAlive(new) )
-        new = m.any()
-        continue
-      #else close enough to reflect on old
-    if old[0].dead:
-      new = fuse(new,old)
-      itsGone(old)
-      remember( itsDead(new) )
-      new = m.any()
-    elif  m.dominates(new,old):
-      k *= The.kMore
-      itsDead(old)
-      remember( itsAlive(new) )
-      new = m.nudge(old,new)
-    elif m.dominates(old,new):
-      remember( itsDead(new) )
-      new = m.nudge(new,old)
-    else:
-      remember( itsAlive(new) )
-      new = m.any()
+print(tbl.cols.cols)
 
-def one234(one, pop, f=lambda x:id(x)): 
-  def oneOther():
-    x = any(pop)
-    while f(x) in seen: 
-      x = any(pop)
-    seen.append( f(x) )
-    return x
-  seen  = [ f(one) ]
-  return oneOther(), oneOther(), oneOther()
-
-def _one234():
-  seed(The.seed)
-  for _ in range(10):
-    print(one234(1,range(1000)))
-
-def de(model=Schaffer):
-  m   = model()
-  frontier = []
-  def remember(new): 
-    m.tell(new); frontier.append(new)
-  def pop0(n) :
-    for i in range(n): 
-      remember( m.any())
-  pop0( The.np * len(m.indep) )
-  lives = The.de.lives
-  while lives > 0:
-    say(lives)
-    lives -= 1
-    better = False
-    for pos,l1 in enumerate(frontier):
-      l2, l3, l4 = one234(l1,frontier)
-      candidate = m.extrapolate(l2,l3,l4)
-      if m.dominates(candidate,l1):
-        say("+")
-        better = True
-        frontier[pos] = candidate
-      elif m.dominates(l1,candidate):
-        True
-      else:
-        remember(candidate)
-    #if better:
-     #  lives += 1
-
-cmd('_close()')
+ 
+#cmd('_close()')
 
